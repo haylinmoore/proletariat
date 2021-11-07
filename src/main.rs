d6645f1b3d278661e037f96606b8c1c4b53cd24d
Hampton Moore <me@hampton.pw>
2021-11-07 18:10:02 -0500

    Whooo!

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,48 @@ use std::sync::mpsc;
 use hyper::{Server};
 use hyper::service::{make_service_fn, service_fn};
 
+#[derive(Clone)]
+struct Request {
+    path: String,
+    body: Option<String>
+}
+
 fn add_event_listener(
     scope: &mut v8::HandleScope,
     args: v8::FunctionCallbackArguments,
     mut _retval: v8::ReturnValue,
 ) {
-    let t = scope.get_slot::<String>().unwrap();
-    print!("Path: {}\n", t);
+    let req = scope.get_slot::<Request>().unwrap().clone();
     if args.get(1).is_function() {
         let func_obj = args.get(1).to_object(scope).unwrap();
         let func = v8::Local::<v8::Function>::try_from(func_obj).unwrap();
-        let args = [v8::Integer::new(scope, 5).into()];
-        let response = func.call(scope, args[0], &args);
+
+        let null = v8::null(scope);
+        // Request Object
+        let values_url = v8::String::new(scope,&req.path.clone()).unwrap();
+        let names_url = v8::String::new(scope,"url").unwrap();
+
+        let names_request = v8::String::new(scope,"request").unwrap();
+        let values_request = v8::Object::with_prototype_and_properties(scope, null.into(), &[names_url.into()], &[values_url.into()]);
+        // Base Object
+        let values_type = v8::String::new(scope,"fetch").unwrap();
+        let names_type = v8::String::new(scope,"type").unwrap();
+
+        let args = [v8::Object::with_prototype_and_properties(scope, null.into(), &[names_type.into(), names_request.into()], &[values_type.into(), values_request.into()]).into()];
+
+        let response = func.call(scope, null.into(), &args);
         match response {
             Some(res) => {
-                if res.is_object() {
+                if res.is_promise() {
+                    print!("Returned promise\n");
+                    let res = v8::Local::<v8::Promise>::try_from(res).unwrap();
+                }
+                else if res.is_object() {
+                    print!("Returned Object\n");
                     let resp = res.to_object(scope).unwrap();
                     send_response_object(scope, resp);
-
-                } else if res.is_function(){
-                    print!("Returned function\n");
-                } else if res.is_promise() {
-                    print!("Returned promise\n");
                 }
+
             }
             None => {
                 print!("Code failed to execute.");
@@ -80,11 +99,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     Ok(())
 }
 
-async fn hello(req: hyper::Request<hyper::Body>) -> Result<hyper::Response<hyper::Body>, Infallible> {
+async fn hello(h_req: hyper::Request<hyper::Body>) -> Result<hyper::Response<hyper::Body>, Infallible> {
     let isolate = &mut v8::Isolate::new(Default::default());
-    let reqpath: String = req.uri().path().to_owned();
-
     let scope = &mut v8::HandleScope::new(isolate);
+    let req = Request {
+        path: h_req.uri().path().to_owned().clone(),
+        body: None
+    };
+
+    scope.set_slot(req.clone());
+
     let context = v8::Context::new(scope);
     let scope = &mut v8::ContextScope::new(scope, context);
 	// first make an "object template" - defining a capability to instance a javascript object such as a "const obj = {}"
@@ -124,15 +148,14 @@ async fn hello(req: hyper::Request<hyper::Body>) -> Result<hyper::Response<hyper
     let scope = &mut v8::ContextScope::new(scope, context);
     let (sender, receiver): (Sender<String>, Receiver<String>) = mpsc::channel();
     scope.set_slot(sender);
-    scope.set_slot(reqpath);
 
     let user_code = String::from("
     function handleRequest(request){
-        return 'Hello ' + request.url;
+        return new Response('Hello ' + request.url);
     }
 
     addEventListener('fetch', event => {
-        return new Response('Hello');
+        return handleRequest(event.request); 
     });
     ");
 
